9096dc692c28df62bba88a6f54cebb277638c626
noa.devanthery <noa.devanthery@he-arc.ch>
2024-12-18 16:03:46 +0100

    change home page text to english

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,17 @@
     <h1>PokeTeams</h1>
     <div class="container my-5">
         <p>
-            Ce site a été conçu lors du cours Developpement Web 1, il permet de créer, gérer et optimiser des équipes de Pokémons :
+            This site was designed during the Web Development 1 course, it allows you to create, manage, and optimize Pokémon teams:
         </p>
         <ul>
-            <li>Créer et personnaliser vos équipes avec vos Pokémon préférés</li>
-            <li>Visualiser leurs forces et faiblesses globales en fonction des types de l'équipe</li>
-            <li>Explorer les statistiques et les forces de chaque Pokémon</li>
-            <li>Rechercher des Pokémon rapidement et découvrir leurs sprites, types, et caractéristiques.</li>
+            <li>Create and customize your teams with your favorite Pokémon</li>
+            <li>View their overall strengths and weaknesses based on team types</li>
+            <li>Explore the stats and strengths of each Pokémon</li>
+            <li>Quickly search for Pokémon and discover their sprites, types, and characteristics.</li>
         </ul>
         <p class="text-center mt-4">
-            <strong>Prêt à commencer ? <a href="/teams" class="btn btn-primary">Créez votre première équipe dès maintenant !</a></strong>
+            <strong>Ready to get started? <a href="/teams" class="btn btn-primary">Create your first team now!</a></strong>
         </p>
     </div>
-
 </div>
 @endsection
